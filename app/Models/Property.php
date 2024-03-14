9e5532ffc5a93449b439abb00a49ed91389aadd0
Tipa Developer <79992131275@ya.ru>
2024-03-14 18:31:51 -0300

    Property model attributes filled.

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -7,5 +7,36 @@ use Illuminate\Database\Eloquent\Model;
 
 class Property extends Model
 {
-    use HasFactory;
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'price' => 'float',
+    ];
+
+    /**
+     * The attributes that should be hidden.
+     *
+     * @var array<string, string>
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'price',
+        'bedrooms',
+        'bathrooms',
+        'storeys',
+        'garages',
+    ];
 }
